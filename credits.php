99190f8a75741a4f590b81464877c18f6b7172c3
Manciukic <rickyman7@gmail.com>
2018-02-15 19:02:30 +0100

    Added credits page and related links and style

---
+++ credits.php
@@ -0,0 +1,26 @@
+<?php
+  session_start();
+  $title = "Asteroids 4.0 - Crediti";
+  $section = 1;
+  $include_css = ['article'];
+  include_once("includes/header.php");
+?>
+<main>
+  <article>
+  <h2>Crediti</h2>
+    <h3>Grafica</h3>
+      <p>Per la grafica si ringrazia <a target="_blank" href="http://www.zekechan.net/about/">Zeke Chan</a> per aver messo a disposizione gratuitamente sul proprio sito tutte le risorse grafiche necessarie: <a target="_blank" href="http://www.zekechan.net/asteroids-html5-game-tutorial-6/">http://www.zekechan.net/asteroids-html5-game-tutorial-6/</a>.</p>
+    <h3>Audio</h3>
+      <p>Per l'audio si ringrazia il sito <a target="_blank" href="http://www.classicgaming.cc">www.classicgaming.cc</a> per aver fornito le risorse audio oridinali di Asteroids al link: <a target="_blank" href="http://www.classicgaming.cc/classics/asteroids/sounds">http://www.classicgaming.cc/classics/asteroids/sounds</a></p>
+      <p>La traccia audio "bonus" o alternativa &egrave; invece stata estratta dal seguente video caricato su YouTube: <a target="_blank" href="https://www.youtube.com/watch?v=8fvTxv46ano">https://www.youtube.com/watch?v=8fvTxv46ano</a>
+    <h3>Font</h3>
+      <p>Il font utilizzato &egrave; <i>Press Start 2P</i> disegnato da CodeMan38 e distribuito con licenza OpenFont all'indirizzo: <a target="_blank" href="https://fonts.google.com/specimen/Press+Start+2P">https://fonts.google.com/specimen/Press+Start+2P</a></p>
+  <h2>Licenza d'uso</h2>
+    <h3>Codice</h3>
+      <p>Il codice &egrave; interamente originale e sviluppato da Riccardo Mancini. Il codice &egrave; interamente open-source e consultabile su GitHub al seguente indirizzo: <a target="_blank" href="https://github.com/Manciukic/Asteroids-4.0">https://github.com/Manciukic/Asteroids-4.0</a></p>
+      <p>Il codice &egrave; distribuito con <a target="_blank" href="https://www.apache.org/licenses/LICENSE-2.0">licenza Apache 2.0</a></p>
+  </article>
+</main>
+<?php
+  include_once("includes/footer.php");
+ ?>
--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
 <footer class="<?php echo (isset($isShortPage) && $isShortPage)?'short-page-footer':'long-page-footer'?>">
-  <p>&copy; 2018 Riccardo Mancini - <a href="privacy.php">Privacy policy</a></p>
+  <p>&copy; 2018 Riccardo Mancini - <a href="privacy.php">Privacy policy</a> - <a href="credits.php">Crediti</a></p>
 </footer>
 </body>
 </html>
