edc0432a25361c7d2b923ae27b4ed0e5d57b69d0
Manciukic <rickyman7@gmail.com>
2018-02-16 23:05:57 +0100

    Reversed last commit, didn't like it :(

--- includes/profile_title_and_menu.php
+++ includes/profile_title_and_menu.php
@@ -12,8 +12,8 @@
   <h2 class="table-title"><?php echo $_SESSION['username'];?></h2>
   <h3 class="table-title"><?php echo $_SESSION['email'];?></h3>
   <ul class = "type-selector">
-    <li><a href="stats.php"   <?php echo (isset($subtitle) && $subtitle=='Statistiche') ?'class="active"':'';?>>Statistiche Utente</a></li>
-    <li><a href="history.php" <?php echo (isset($subtitle) && $subtitle=='Cronologia')  ?'class="active"':'';?>>Cronologia Partite</a></li>
+    <li><a href="stats.php"   <?php echo (isset($subtitle) && $subtitle=='Statistiche') ?'class="active"':'';?>>Statistiche</a></li>
+    <li><a href="history.php" <?php echo (isset($subtitle) && $subtitle=='Cronologia')  ?'class="active"':'';?>>Cronologia</a></li>
     <li><a href="edit.php"    <?php echo (isset($subtitle) && $subtitle=='Modifica')    ?'class="active"':'';?>>Modifica password</a></li>
     <li><a href="delete.php"  <?php echo (isset($subtitle) && $subtitle=='Elimina')    ?'class="active"':'';?>
            onclick="return confirm('Questa azione non pu\u00f2 essere annullata, continuare?');">Elimina account</a></li>
