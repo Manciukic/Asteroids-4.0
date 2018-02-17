9aef7fd8e07de7eec56300ebec8622cc57726759
Manciukic <rickyman7@gmail.com>
2018-02-17 11:10:19 +0100

    Reduced button width to 75%

--- exit.php
+++ exit.php
@@ -4,7 +4,7 @@
 
   $_SESSION['login'] = false;
   $title = "Asteroids 4.0 - Logout";
-  $message_text = "Logout avvenuto con successo";
+  $message_text = "Logout avvenuto con successo!";
   $message_href = "login.php";
   $message_button = "Accedi";
   include_once("includes/message.php");
